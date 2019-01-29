73ba67359c34d667e0abde2e672a33d456b361c5
ADaanCruz <djdaan86@gmail.com>
2019-01-29 08:43:11 -0600

    All Visual - Teacher - 2
    All Visual - Teacher - 2

--- app/src/main/java/com/adancruz/cedehaaapp/TeCoursesFragment.java
+++ app/src/main/java/com/adancruz/cedehaaapp/TeCoursesFragment.java
@@ -7,12 +7,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TeCoursesFragment extends Fragment {
+
+    ListView cursos;
+    ArrayList<Curso> arrayList = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_te_courses, container, false);
+        final View view = inflater.inflate(R.layout.fragment_te_courses, container, false);
+
+        cursos = (ListView) view.findViewById(R.id.lista_te_cursos);
+        cursos.setAdapter(new ListCoursesAdapter(view.getContext(), GetArrayItems()));
+
+        return view;
+    }
+
+    private ArrayList<Curso> GetArrayItems() {
+        ArrayList<Curso> listItems = new ArrayList<>();
+
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 1",
+                "Descripción 1", "Desc General 1"));
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 2",
+                "Descripción 2", "Desc General 2"));
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 3",
+                "Descripción 3", "Desc General 3"));
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 4",
+                "Descripción 4", "Desc General 4"));
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 5",
+                "Descripción 5", "Desc General 5"));
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 6",
+                "Descripción 6", "Desc General 6"));
+        listItems.add(new Curso(R.drawable.placeholder, "Titulo 7",
+                "Descripción 7", "Desc General 7"));
+
+        return listItems;
     }
 }
--- app/src/main/java/com/adancruz/cedehaaapp/TeNotificationsFragment.java
+++ app/src/main/java/com/adancruz/cedehaaapp/TeNotificationsFragment.java
@@ -7,12 +7,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TeNotificationsFragment extends Fragment {
+
+    ListView notificaciones;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_te_notifications, container, false);
+        final View view = inflater.inflate(R.layout.fragment_te_notifications, container, false);
+
+        notificaciones = (ListView) view.findViewById(R.id.lista_te_notificaciones);
+        notificaciones.setAdapter(new ListNotificationsAdapter(view.getContext(), GetArrayItems()));
+
+        return view;
+    }
+
+    private ArrayList<Notificacion> GetArrayItems() {
+        ArrayList<Notificacion> listItems = new ArrayList<>();
+
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 1",
+                "Grupo 1", "Profesor", "Usuario 1",
+                "Mensaje 1", "dd/mm/yy"));
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 2",
+                "Grupo 2", "Profesor", "Usuario 2",
+                "Mensaje 2", "dd/mm/yy"));
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 3",
+                "Grupo 3", "Profesor", "Usuario 3",
+                "Mensaje 3", "dd/mm/yy"));
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 4",
+                "Grupo 4", "Profesor", "Usuario 4",
+                "Mensaje 4", "dd/mm/yy"));
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 5",
+                "Grupo 5", "Profesor", "Usuario 5",
+                "Mensaje 5", "dd/mm/yy"));
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 6",
+                "Grupo 6", "Profesor", "Usuario 6",
+                "Mensaje 6", "dd/mm/yy"));
+        listItems.add(new Notificacion(R.drawable.placeholder, "Curso 7",
+                "Grupo 7", "Profesor", "Usuario 7",
+                "Mensaje 7", "dd/mm/yy"));
+
+        return listItems;
     }
 }
